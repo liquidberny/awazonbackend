1472b0528338f0c51c4f54cdbea2b6b1da930456
AngelLaines <angelmoreno755@gmail.com>
2023-03-07 14:49:55 -0700

    correcion linea 4 carrier

--- api/Carrier.js
+++ api/Carrier.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Carrier = require("../models/Carrier");
-const User = require("../models/User");
 
 //contrasena
 const bcrypt = require('bcrypt');
