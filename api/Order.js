a6dacef19017d9abd972b78a89d9c2f33c1b8c96
bernardo.urquijo <bernardo.urquijo@strategix4.com>
2023-04-10 17:01:12 -0700

    crear orden, no se si sirva lol

--- api/Order.js
+++ api/Order.js
@@ -2,7 +2,52 @@ const express = require("express");
 const router = express.Router();
 const Order = require("../models/Order");
 
-//codigo de endpoints aqui: 
+//crear orden
+router.post('/create', (req, res) => {
+    let id_client = req.body.id_client;
+    let id_carrier = req.body.id_carrier
+    let cant_garrafones = req.body.cant_garrafones
+    let cuota_servicio = req.body.cuota_servicio
+    let total = req.body.total
+    let orden_status = req.body.orden_status;
+    let entraga_status = req.body.entraga_status;
+    let fecha_pedido = req.body.fecha_pedido;
+    let fecha_entrega = req.body.fecha_entrega;
 
+    if (id_client == "" || id_carrier == "" || cant_garrafones == "" || cuota_servicio == "" ||
+        orden_status == "" || entraga_status == "" || fecha_pedido == "" || fecha_entrega == "") {
+        res.json({
+            status: "FAILED",
+            message: "Empty input fields!"
+        });
+    } else {
+        // Try to create new order
+        const newOrder = new Order({
+            id_client,
+            id_carrier,
+            cant_garrafones,
+            cuota_servicio,
+            total,
+            orden_status,
+            entraga_status,
+            fecha_pedido,
+            fecha_entrega,
+        });
+
+        newOrder.save().then(result => {
+            res.json({
+                status: "SUCCESS",
+                message: "Order created successfully!",
+                data: result
+            })
+        })
+            .catch(err => {
+                res.json({
+                    status: "FAILED",
+                    message: "An error occurred while creating order"
+                })
+            })
+    }
+});
 
 module.exports = router;
