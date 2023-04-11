d2c9cf8e6457cfbbbe0dceb3867ce0c241ec1277
AngelLaines <angelmoreno755@gmail.com>
2023-04-11 11:56:58 -0700

    consultar orden client y carrier 2

--- api/Order.js
+++ api/Order.js
@@ -99,4 +99,50 @@ router.put('/:orderId/Cancelarorden',async (req, res) => {
       });
 
 });
+
+
+// Consultar orden - cliente
+router.get("/read/client/:id", async (req, res) => {
+    const id = req.params.id;
+
+    Order.find({ id_client: id, orden_status: "PENDIENTE" }).then(result => {
+        console.log('order');
+        if (result.length !== 0) {
+            res.json({
+                status: "SUCCESS",
+                message: "Order successfully obtained",
+                data: result
+            });
+        }
+    }).catch(err => {
+        console.log(err);
+        res.json({
+            status: "FAILED",
+            message: "An error ocurred while checking for existing order!"
+        })
+    });
+
+});
+// Consultar orden - carrier
+router.get("/read/carrier/:id", async (req, res) => {
+    const id = req.params.id;
+
+    Order.find({ id_carrier: id, orden_status: "PENDIENTE" }).then(result => {
+        console.log('order');
+        if (result.length !== 0) {
+            res.json({
+                status: "SUCCESS",
+                message: "Order successfully obtained",
+                data: result
+            });
+        }
+    }).catch(err => {
+        console.log(err);
+        res.json({
+            status: "FAILED",
+            message: "An error ocurred while checking for existing order!"
+        })
+    });
+
+});
 module.exports = router;
